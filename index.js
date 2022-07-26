e5eb1c9aacf30ec63bb3489bbdc4ee8bfd88ee10
Antonio <antonio18casta@aol.com>
2022-07-26 16:54:11 -0400

    commit tree ca00ccb44afa80a5f12ce2e468d888779a4c3218

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(firstName, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${firstName}`,
+            email: `${email}`,
+        })
+    };
+    
+    return fetch("http://localhost:3000/users", configObj)
+    .then(res => res.json())
+    .then(obj => document.body.innerHTML = obj.id)
+    .catch(error => document.body.innerHTML = error.message)
+};
